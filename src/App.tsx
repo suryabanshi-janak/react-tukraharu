b17f658f5393b13398d5534bbc599d9ef29a1491
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2024-08-25 18:49:28 +0545

    feat: sidebar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import RecursiveFileTree from './components/RecursiveFileTree';
+// import RecursiveFileTree from './components/RecursiveFileTree';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
-    <div className='max-w-5xl mx-auto my-20'>
-      <RecursiveFileTree />
-    </div>
+    // <div className='max-w-5xl mx-auto my-20'>
+    //   {/* <RecursiveFileTree /> */}
+    // </div>
+    <Sidebar />
   );
 }
 
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,162 @@
+import { useState } from 'react';
+import { motion, Variants } from 'framer-motion';
+import {
+  ChartBarIcon,
+  ChartPieIcon,
+  DocumentCheckIcon,
+  Square2StackIcon,
+  UsersIcon,
+} from '@heroicons/react/24/outline';
+
+const containerVariants: Variants = {
+  closed: {
+    width: '5rem',
+    transition: {
+      type: 'spring',
+      damping: 15,
+      duration: 0.5,
+    },
+  },
+  open: {
+    width: '16rem',
+    transition: {
+      type: 'spring',
+      damping: 15,
+      duration: 0.5,
+    },
+  },
+};
+
+const svgVariants: Variants = {
+  closed: {
+    rotate: 360,
+  },
+  open: {
+    rotate: 180,
+  },
+};
+
+const navVariants: Variants = {
+  closed: {
+    height: 0,
+    opacity: 0,
+  },
+  open: {
+    height: 'auto',
+    opacity: 1,
+  },
+};
+
+export default function Sidebar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  // const containerControls = useAnimationControls();
+  // const svgControls = useAnimationControls();
+
+  // useEffect(() => {
+  //   if (isOpen) {
+  //     containerControls.start('open');
+  //     svgControls.start('open');
+  //   } else {
+  //     containerControls.start('closed');
+  //     svgControls.start('closed');
+  //   }
+  // }, [containerControls, isOpen, svgControls]);
+
+  const onToggleSidebar = () => setIsOpen((prev) => !prev);
+
+  return (
+    <main className='relative flex flex-row w-full h-screen'>
+      {/* SIDEBAR */}
+      <motion.aside
+        variants={containerVariants}
+        animate={isOpen ? 'open' : 'closed'}
+        initial='closed'
+        className='absolute top-0 left-0 z-10 flex flex-col w-20 h-full gap-20 p-5 shadow bg-neutral-900 shadow-neutral-600'
+      >
+        <div className='flex flex-row justify-between w-full place-items-center'>
+          <div className='w-10 h-10 rounded-full bg-gradient-to-br from-orange-500 to-amber-700' />
+          <button className='flex p-1 rounded-full' onClick={onToggleSidebar}>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              strokeWidth={1}
+              stroke='currentColor'
+              className='w-8 h-8 stroke-neutral-200'
+            >
+              <motion.path
+                variants={svgVariants}
+                animate={isOpen ? 'open' : 'closed'}
+                strokeLinejoin='round'
+                d='M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3'
+                transition={{
+                  duration: 0.5,
+                  ease: 'easeInOut',
+                }}
+              />
+            </svg>
+          </button>
+        </div>
+
+        <div className='flex flex-col gap-3'>
+          <NavigationLink name='Dashboard' isOpen={isOpen}>
+            <ChartBarIcon className='stroke-inherit stroke-[0.75] min-w-8 w-8' />
+          </NavigationLink>
+          <NavigationLink name='Projects' isOpen={isOpen}>
+            <Square2StackIcon className='stroke-inherit stroke-[0.75] min-w-8 w-8' />
+          </NavigationLink>
+          <NavigationLink name='Tasks' isOpen={isOpen}>
+            <DocumentCheckIcon className='stroke-inherit stroke-[0.75] min-w-8 w-8' />
+          </NavigationLink>
+          <NavigationLink name='Reporting' isOpen={isOpen}>
+            <ChartPieIcon className='stroke-inherit stroke-[0.75] min-w-8 w-8' />
+          </NavigationLink>
+          <NavigationLink name='Users' isOpen={isOpen}>
+            <UsersIcon className='stroke-inherit stroke-[0.75] min-w-8 w-8' />
+          </NavigationLink>
+        </div>
+      </motion.aside>
+
+      {/* MAIN CONTENT */}
+      <section className='flex flex-col w-full gap-5 p-10 ml-20'>
+        <h1 className='text-4xl text-neutral-200'>Dashboard</h1>
+        <div className='w-full border rounded h-80 border-neutral-500/50 bg-neutral-800/20' />
+        <div className='flex flex-row w-full gap-5'>
+          <div className='w-1/2 border rounded border-neutral-500/50 h-60 bg-neutral-800/20' />
+          <div className='w-1/2 border rounded border-neutral-500/50 h-60 bg-neutral-800/20' />
+        </div>
+      </section>
+    </main>
+  );
+}
+
+function NavigationLink({
+  children,
+  name,
+  isOpen,
+}: {
+  children: React.ReactNode;
+  name: string;
+  isOpen: boolean;
+}) {
+  return (
+    <a
+      href='#'
+      className='flex p-1 rounded cursor-pointer stroke-[0.75] hover:stroke-neutral-100 stroke-neutral-400 text-neutral-400 hover:text-neutral-100 place-items-center gap-3 hover:bg-neutral-700/30 transition-colors duration-100'
+    >
+      {children}
+      <motion.p
+        variants={navVariants}
+        animate={isOpen ? 'open' : 'closed'}
+        transition={{
+          duration: 0.5,
+          ease: 'easeInOut',
+        }}
+        className='tracking-wide text-inherit font-poppins overflow-clip whitespace-nowrap'
+      >
+        {isOpen ? name : ''}
+      </motion.p>
+    </a>
+  );
+}
